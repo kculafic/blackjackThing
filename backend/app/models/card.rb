d4213f82ea64ff7deb52799e20ff4b352d765881
Krsto Culafic <kculafic@gmail.com>
2025-11-07 16:00:21 -0600

    Add validations,`value` & `display_name` methods to Card model

--- backend/app/models/card.rb
+++ backend/app/models/card.rb
@@ -1,4 +1,22 @@
 class Card < ApplicationRecord
   SUITS = %w[hearts diamonds clubs spades].freeze
   RANKS = %w[A 2 3 4 5 6 7 8 9 10 J Q K].freeze
+
+  validates :suit, presence: true, inclusion: { in: SUITS }
+  validates :rank, presence: true, inclusion: { in: RANKS }
+
+  def value
+    case rank
+    when 'A'
+      11
+    when 'J', 'Q', 'K'
+      10
+    else
+      rank.to_i
+    end
+  end
+
+  def display_name
+    "#{rank} of #{suit}"
+  end
 end
