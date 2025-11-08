de46b915331bf273d89fbb95926c5f894ee56aa8
Krsto Culafic <kculafic@gmail.com>
2025-11-08 11:58:53 -0600

    Add dealer methods % gameplay logic to Game model

--- backend/app/models/game.rb
+++ backend/app/models/game.rb
@@ -21,4 +21,47 @@ class Game < ApplicationRecord
       save
     end
   end
+
+  def hit
+    card = deck.draw
+    player_hand.add_card(card)
+    player_hand.save
+
+    if player_hand.calculate_value > 21
+      self.status = 'player_busted'
+      save
+    end
+  end
+
+  def stand
+    self.status = 'player_stood'
+    save
+    dealer_play
+  end
+
+  def dealer_play
+    while dealer_hand.calculate_value < 17
+      dealer_hand.add_card(deck.draw)
+      dealer_hand.save
+    end
+
+    determine_winner
+  end
+
+  def determine_winner
+    player_value = player_hand.calculate_value
+    dealer_value = dealer_hand.calculate_value
+
+    if dealer_value > 21
+      self.status = 'player_wins'
+    elsif player_value > dealer_value
+      self.status = 'player_wins'
+    elsif dealer_value > player_value
+      self.status = 'dealer_wins'
+    else
+      self.status = 'push'
+    end
+
+    save
+  end
 end
